d4546f5321133ba0dbbed2ac5e9b96673a7219d3
Samuel Banya <sbanya@fastmail.com>
2022-09-24 16:44:28 -0500

    Adding Checklist route

--- smoothmoves-frontend/src/App.js
+++ smoothmoves-frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<About />}/>
         <Route path="/form" element={<Form />}/>
+        <Route path="/checklist" element={<Checklist />}/>
       </Routes>
     </div>
   );
