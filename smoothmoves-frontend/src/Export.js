eb0f8b4312d4f51795ea7f47500d0c03c726fe8d
Samuel Banya <sbanya@fastmail.com>
2022-09-27 19:31:12 -0500

    Adding latest changes to Export component

--- smoothmoves-frontend/src/Export.js
+++ smoothmoves-frontend/src/Export.js
@@ -1,6 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 function Export() {
+    const [moves, setMoves] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:/moves")
+        .then(response => response.json())
+        .then(data => {
+            setMoves(data);
+        })
+    }, [])
+
+    return (
+    <div>
+    <h2>Export</h2>
+    <form>
+        <div>
+            <label htmlFor="specificMove">Choose A Move</label>
+            <select onChange={setExport} id="exportSelect" name="exportSelect">
+                { 
+                    moves.forEach(move) => {
+                        <option>{move}</option>
+                    }
+                }
+            </select>
+        </div>
+        <br />
+        <button className="exportButton" type="submit">Export</button>
+    </form>
+</div>
+    )
 
 }
 
