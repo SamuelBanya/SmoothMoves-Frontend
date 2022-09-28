f366d27e79914e31ce54dc4ab0bc28191891d19f
Samuel Banya <sbanya@fastmail.com>
2022-09-27 19:36:53 -0500

    Adding changes to Email component to include React controlled form components

--- smoothmoves-frontend/src/Email.js
+++ smoothmoves-frontend/src/Email.js
@@ -1,29 +1,33 @@
 import React from "react";
 
 function Email() {
+    function handleNameChange() {
+
+    }
+
+    function handleEmailChange() {
+
+    }
+
     return (
         <div>
             <h2>Form</h2>
             <form>
                 <div>
-                    <label htmlFor="pickupLocation"></label>
-                    <input />
-                    <label htmlFor="dropoffLocation"></label>
-                    <input />
-                    // NOTE: I need to iterate through the amount of items
-                    // that the user wants to enter, and therefore ask
-                    // them the following details for each item:
-                    // Height, width, length (inches, later converted to square feet)
-                    // Weight (lbs, pounds)
-                    <label htmlFor="itemAmount"></label>
-                    <input />
+                    <label htmlFor="moverName"></label>
+                    <input 
+                        onChange={handleNameChange}
+                    />
+                    <label htmlFor="moverEmail"></label>
+                    <input
+                        onChange={handleEmailChange}
+                    />
                 </div>
                 <br />
-                <button className="uploadButton" type="submit">Upload</button>
+                <button className="emailButton" type="submit">Upload</button>
             </form>
         </div>
         )
-
 }
 
 export default Email;
