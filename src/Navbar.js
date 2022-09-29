f655d69714d0618d566386ae71f356eb648b8089
Samuel Banya <sbanya@fastmail.com>
2022-09-28 21:33:23 -0500

    Adding NavBar from other project to ensure it's working

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const linkStyles = {
+    display: "inline-block",
+    width: "60px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "#00fc8f",
+    textDecoration: "none",
+    fontWeight: "bold",
+    color: "black",
+  };
 
 function NavBar() {
     return (
