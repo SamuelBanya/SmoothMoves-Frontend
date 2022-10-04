b8c8e3118ba11267ade00247cbb9ec7dbbd9862e
Samuel Banya <sbanya@fastmail.com>
2022-10-04 16:14:40 -0500

    Adding changes to MoveForm child component for 'Edit' functionality

--- src/App.js
+++ src/App.js
@@ -8,30 +8,29 @@ import Checklist from "./Checklist"
 import Export from "./Export"
 import Email from "./Email"
 import NavBar from "./NavBar"
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  // const [moveList, setMoveList] = useState([{pickupLocation: "", dropoffLocation: ""}]);
-
-  const [moveFormData, setMoveFormData] = useState({
+  // CREATE:
+  const [createMoveFormData, setCreateMoveFormData] = useState({
     pickupLocation: "", 
     dropoffLocation: ""
   });
 
-  console.log("moveFormData (from parent App component): ", moveFormData);
+  console.log("createMoveFormData (from parent App component): ", createMoveFormData);
 
-  const handleMoveChange = (e) => {
+  const handleCreateMoveChange = (e) => {
       // NOTE: This is the same as using obj2 = {...obj1, name: "Sam"}
       // This is because we are utilizing the existing data by using the spread operator:
-      setMoveFormData({...moveFormData, [e.target.name]: e.target.value});
+      setCreateMoveFormData({...createMoveFormData, [e.target.name]: e.target.value});
   };
 
-  const handleMoveFormSubmit = (e) => {
+  const handleCreateMoveFormSubmit = (e) => {
     e.preventDefault();
     // NOTE: This is the single lin that is causing the issue I am having since I need to just create an individual move based object, not a list of them
     // setMoveList([moveFormData, ...moveList]);
     // setMoveFormData(moveFormData);
-    console.log("moveFormData inside POST request: ", moveFormData);
+    console.log("createMoveFormData inside POST request: ", createMoveFormData);
     fetch("http://localhost:9292/moves", {
       method: "POST",
       headers: {
@@ -43,7 +42,7 @@ function App() {
 
       // Related MDN reference:
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
-      body: JSON.stringify({ "pickup_location": moveFormData["pickupLocation"], "dropoff_location": moveFormData["dropoffLocation"] }),
+      body: JSON.stringify({ "pickup_location": createMoveFormData["pickupLocation"], "dropoff_location": createMoveFormData["dropoffLocation"] }),
     })
     .then((response) => response.json())
     .then((data) => { 
@@ -56,13 +55,40 @@ function App() {
       // });
     });
   }
+  // EDIT:
+  useEffect(() => {
+    console.log("Using useEffect() hook to make a GET request");
+    fetch("http://localhost:9292/moves", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data: ", data);
+    })
+  }, [])
+  const [editMoveFormData, setEditMoveFormData] = useState([]);
+
+  const handleEditMoveChange = (e) => {
+    console.log("handleEditMoveChange() function called in parent App component");
+  }
+
+  const handleEditMoveFormSubmit = (e) => {
+  }
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<About />}/>
-        <Route path="/moves" element={<MoveForm handleMoveFormSubmit={handleMoveFormSubmit} moveFormData={moveFormData} handleMoveChange={handleMoveChange} />}/>
+        <Route path="/moves" element={<MoveForm 
+          handleCreateMoveFormSubmit={handleCreateMoveFormSubmit} createMoveFormData={createMoveFormData} handleCreateMoveChange={handleCreateMoveChange} 
+          handleEditMoveFormSubmit={handleEditMoveFormSubmit} editMoveFormData={editMoveFormData} handleEditMoveChange={handleEditMoveChange}
+        />}
+        />
         <Route path="/items" element={<ItemForm />}/>
         <Route path="/checklist" element={<Checklist />}/>
         <Route path="/export" element={<Export />}/>
--- src/MoveForm.js
+++ src/MoveForm.js
@@ -2,14 +2,19 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 
-function MoveForm({handleMoveFormSubmit, moveFormData, handleMoveChange}) {
-    console.log("\n\nmoveFormData sent down as props from App component to child MoveForm component: ")
-    console.log("moveFormData: ", moveFormData)
+function MoveForm({handleCreateMoveFormSubmit, createMoveFormData, handleCreateMoveChange, handleEditMoveFormSubmit, editMoveFormData, handleEditMoveChange}) {
+    console.log("\n\ncreateMoveFormData sent down as props from App component to child MoveForm component: ")
+    console.log("createMoveFormData: ", createMoveFormData)
+    let age = 0;
     return (
         <div>
             <h2>Create New Move</h2>
-            <form onSubmit={handleMoveFormSubmit}>
+            <form onSubmit={handleCreateMoveFormSubmit}>
                 <Grid container alignItems="center" justify="center" direction="column">
                     <Grid item>
                         <TextField 
@@ -17,8 +22,8 @@ function MoveForm({handleMoveFormSubmit, moveFormData, handleMoveChange}) {
                             name="pickupLocation"
                             label='Pickup Location (ex: "New York, NY")'
                             type="text"
-                            value={moveFormData.pickupLocation}
-                            onChange={handleMoveChange}
+                            value={createMoveFormData.pickupLocation}
+                            onChange={handleCreateMoveChange}
                         />
                     </Grid>
                     <br />
@@ -28,14 +33,62 @@ function MoveForm({handleMoveFormSubmit, moveFormData, handleMoveChange}) {
                             name="dropoffLocation"
                             label='Dropoff Location (ex: "Orlando, FL")'
                             type="text"
-                            value={moveFormData.dropoffLocation}
-                            onChange={handleMoveChange}
+                            value={createMoveFormData.dropoffLocation}
+                            onChange={handleCreateMoveChange}
                         />
                     </Grid>
                     <br />
-                    <Button variant="contained" color="primary" type="submit">Submit</Button>
+                    <Button variant="contained" color="primary" type="submit">Create</Button>
                 </Grid>
             </form>
+            <br />
+            <h2>Edit Existing Move</h2>
+            <form onSubmit={handleEditMoveFormSubmit}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    // value={age}
+                    label="Age"
+                    onChange={handleEditMoveChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+                <br />
+                <br />
+                <Grid container alignItems="center" justify="center" direction="column">
+                    <Grid item>
+                        <TextField 
+                            id="pickupLocation"
+                            name="pickupLocation"
+                            label='Pickup Location (ex: "New York, NY")'
+                            type="text"
+                            // value={editMoveFormData.pickupLocation}
+                            onChange={handleEditMoveChange}
+                        />
+                    </Grid>
+                    <br />
+                    <Grid item>
+                        <TextField 
+                            id="dropoffLocation"
+                            name="dropoffLocation"
+                            label='Dropoff Location (ex: "Orlando, FL")'
+                            type="text"
+                            // value={editMoveFormData.dropoffLocation}
+                            onChange={handleEditMoveChange}
+                        />
+                    </Grid>
+                    <br />
+                    <Button variant="contained" color="primary" type="submit">Update</Button>
+                    <br />
+                    <Button variant="contained" color="primary" type="submit">Delete</Button>
+                </Grid>
+            </form>
+
         </div>
         )
 }
