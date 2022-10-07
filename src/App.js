ddd84781890b7d71cc64ea3d3e01a92195a3bf86
Samuel Banya <sbanya@fastmail.com>
2022-10-06 19:22:52 -0500

    In the process of refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from "react-router-dom"
-import About from "./About"
-import MoveForm from "./MoveForm"
-import ItemForm from "./ItemForm"
-import NavBar from "./NavBar"
+import { Routes, Route } from "react-router-dom";
+import About from "./components/generalComponents/About";
+import MoveForm from "./components/moveComponents/MoveForm";
+import NavBar from "./components/generalComponents/NavBar";
 import React, { useState, useEffect } from "react";
 
 function App() {
@@ -35,18 +34,11 @@ function App() {
       body: JSON.stringify({ "pickup_location": createMoveFormData["pickup_location"], "dropoff_location": createMoveFormData["dropoff_location"] }),
     })
     .then((response) => response.json())
-    .then((data) => { 
-      // NOTE: This clears the form data upon each submission:
-      // console.log("Clearing form...")
-      // setMoveFormData({
-      //     pickupLocation: "", 
-      //     dropoffLocation: ""
-      // });
-    });
   }
   // EDIT MOVE SECTION:
   // NOTE: This is to determine the values of the '<Select>' tags:
-  const [selectValues, setSelectValues] = useState([]);
+  // const [selectValues, setSelectValues] = useState([]);
+  const [moves, setMoves] = useState([]);
   // NOTE: This is to pass down the specific value of the given '<Select>' tag to render it properly:
   const [selectTagValue, setSelectTagValue] = useState("");
   // NOTE: This is to provide the specific element value so that the related 'pickupLocation' and 'dropoffLocation' values can be displayed properly
@@ -65,7 +57,7 @@ function App() {
   // NOTE: A big part of this also depends upon the 'useEffect' section at the very bottom
   // since the useEffect() hook is using all the variables placed before it
 
-  // NOTE: This is important to mention because the 'setSelectValues(data);' statement is
+  // NOTE: This is important to mention because the 'setMoves(data);' statement is
   // actually set during the useEffect() hook itself
   // Here, we are using 'selectTagValue' and 'setSelectTagValue' in tandem so that we can
   // determine the 'id' of the specific element we want to manipulate later for the 'EDIT' section:
@@ -74,7 +66,10 @@ function App() {
     console.log("e: ", e);
     console.log("e.target.value: ", e.target.value);
     setSelectTagValue(e.target.value);
-    setToggle(!toggle);
+    // setToggle(!toggle);
+    let pickupLocationTag = document.getElementById("#edit_pickup_location");
+    console.log("pickupLocationTag: ", pickupLocationTag);
+    setEditMoveFormData(moves.find(element => element["id"] === e.target.value));
   }
 
   const handleUpdateMove = (e) => {
@@ -91,10 +86,7 @@ function App() {
     console.log(editMoveFormData ? editMoveFormData : "");
     console.log("editMoveFormData.id: ")
     console.log(editMoveFormData ? editMoveFormData.id : "");
-    // TODO: fetch() request for 'DELETE' action for backend needed
-    // Pass :id value using Active Record methods
-    // Related Docs page:
-    // https://apidock.com/rails/ActiveRecord/Relation/delete
+
     fetch(`http://localhost:9292/moves/${editMoveFormData.id}`, {
       method: "DELETE",
       headers: {
@@ -109,64 +101,9 @@ function App() {
   }
 
   // =============================================
-  // ITEM COMPONENT SECTION:
-  // =============================================
-  const [itemMoveSelectValues, setItemMoveSelectValues] = useState([]);
-  const [itemMoveSelectTagValue, setItemMoveSelectTagValue] = useState("");
-  const [itemAmount, setItemAmount] = useState(0);
-
-  const handleItemSubmit = (e) => {
-    e.preventDefault();
-    console.log("handleItemSubmit() function called");
-    setItemAmount(e.target[0].value);
-  }
-
-  function handleItemMoveSelectTagChange(e) {
-    console.log("handleItemMoveSelectTagChange() function called in App parent component")
-    console.log("e: ", e);
-    console.log("e.target.value: ", e.target.value);
-    setItemMoveSelectTagValue(e.target.value);
-  }
-
-  const handleCarouselChange = (e) => {
-    console.log("handleCarouselChange() function called");
-  }
-
-  // =============================================
   // USE EFFECT SECTION
   // =============================================
-  const [toggle, setToggle] = useState(false);
-
-  // useEffect() call to set the <Select> tag values for the 'MoveForm' and 'ItemForm' components:
-  useEffect(() => {
-    fetch("http://localhost:9292/moves", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      setSelectValues(data);
-      setItemMoveSelectValues(data);
-    })
-  }, []);
-
-  // useEffect() call to determine the values to use for the 'editFormData' to prepopulate data on the 'MoveForm' component
-  useEffect(() => {
-    fetch("http://localhost:9292/moves", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      setEditMoveFormData(data.find(element => element["id"] === selectTagValue));
-    })
-  }, [toggle]);
+  // const [toggle, setToggle] = useState(false);
 
   return (
     <div className="App">
@@ -177,7 +114,7 @@ function App() {
           element={<MoveForm 
             handleCreateMoveFormSubmit={handleCreateMoveFormSubmit} createMoveFormData={createMoveFormData} handleCreateMoveChange={handleCreateMoveChange} 
             editMoveFormData={editMoveFormData} handleEditMoveChange={handleEditMoveChange}
-            selectValues={selectValues} handleSelectTagChange={handleSelectTagChange} selectTagValue={selectTagValue} handleUpdateMove={handleUpdateMove}
+            moves={moves} handleSelectTagChange={handleSelectTagChange} selectTagValue={selectTagValue} handleUpdateMove={handleUpdateMove}
             handleDeleteMove={handleDeleteMove}
           />}
         />
