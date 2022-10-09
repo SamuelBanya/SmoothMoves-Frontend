edfc98b86eef7f80e053b0a8f49914a227969c3b
Samuel Banya <sbanya@fastmail.com>
2022-10-08 21:41:30 -0500

    Refactoring "ItemsForm" component to be more inclusive

--- src/components/itemComponents/ItemsForm.js
+++ src/components/itemComponents/ItemsForm.js
@@ -2,7 +2,163 @@ import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import ItemsCarouselForm from "./ItemsCarouselForm";
+import Carousel from "react-material-ui-carousel";
+import Select from "@mui/material/Select";
+import NativeSelect from "@mui/material/NativeSelect"
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+
+function ItemCard({ id }) {
+    const [itemFormData, setItemFormData] = useState({
+        name: "",
+        length: 0, 
+        width: 0,
+        height: 0,
+        weight: 0
+    });
+
+    function handleItemFormChange(e) {
+        const name = e.target.name;
+        let value = e.target.value;
+        console.log("handleItemFormChange() function called in ItemCard child component: ")
+        console.log("name: ", name);
+        console.log("value: ", value);
+
+        setItemFormData({
+            ...itemFormData,
+            [name]: value,
+        });
+    }
+
+    return (
+        <div>
+            <h2>Item # {id + 1}</h2>
+            <Button variant="contained" color="primary" type="submit">Delete</Button>
+            <br />
+            <br />
+            <Grid container alignItems="center" justify="center" direction="column">
+                <Grid item>
+                    <TextField 
+                        id="name"
+                        name="name"
+                        label="Name"
+                        type="string"
+                        onChange={handleItemFormChange}
+                        value={itemFormData.name}
+                    />
+                </Grid>
+                <br />
+                <Grid item>
+                    <TextField 
+                        id="length"
+                        name="length"
+                        label="Length (in.)"
+                        type="number"
+                        onChange={handleItemFormChange}
+                        value={itemFormData.length}
+                    />
+                </Grid>
+                <br />
+                <Grid item>
+                    <TextField 
+                        id="width"
+                        name="width"
+                        label="Width (in.)"
+                        type="number"
+                        onChange={handleItemFormChange}
+                        value={itemFormData.width}
+                    />
+                </Grid>
+                <br />
+                <Grid item>
+                    <TextField 
+                        id="height"
+                        name="height"
+                        label="Height (in.)"
+                        type="number"
+                        onChange={handleItemFormChange}
+                        value={itemFormData.height}
+                    />
+                </Grid>
+                <br />
+                <Grid item>
+                    <TextField 
+                        id="weight"
+                        name="weight"
+                        label="Weight (lbs.)"
+                        type="number"
+                        onChange={handleItemFormChange}
+                        value={itemFormData.weight}
+                    />
+                </Grid>
+                <br />
+            </Grid>
+        </div>
+    )
+}
+
+function ItemsCarouselForm({ moves, itemAmount, itemMoveSelectTagValue }) {
+
+    function handleCarouselChange() {
+        console.log("handleCarouselChange() function called");
+    }
+
+    const handleItemsCarouselFormSubmit = (e) => {
+        console.log("handleItemsCarouselFormSubmit() function called");
+        console.log("e: ", e);
+        console.log("itemsCarouselArray: ", itemsCarouselArray);
+        // NOTE: Use the existing 'moves' data, and index into it so that you can utilize 'itemMoveSelectTagValue' 
+        // to make the appropriate fetch() request accordingly, and figure out how to create the appropriate fetch() request accordingly
+        console.log("moves: ", moves);
+        console.log("itemMoveSelectTagValue from child ItemsCarouselForm component: ", itemMoveSelectTagValue);
+        // Make a 'GET' fetch() request to the '/moves/:id/items' API endpoint so that you can associate 'items' with a given 'move' instance:
+        // post "/moves/:id/items" do 
+
+        // NOTE: I have to loop through each of the amount of items in 'itemAmount' so that I can 
+        // make a 'POST' request for each individual item accordingly:
+
+        // TODO: I need to somehow get the individual data from each 'ItemCard' component in the form of an array of
+        // 'itemFormData' objects that contain each specific item's data so that I can make individual POST 
+        // requests accordingly:
+        console.log("Test for loop within handleItemsCarouselFormSubmit before fetch() based POST request to create items for each 'move' instance: ")
+        for (let i = 0; i < itemAmount; i++) {
+            console.log("i: ", i);
+            // fetch(`http://localhost:9292/${itemMoveSelectTagValue}/items`, {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //         "Accept": "application/json",
+            //     },
+            //     body: JSON.stringify()
+            // })
+            // .then((response) => response.json())
+        }
+    }
+
+    let itemsCarouselArray = [];
+
+    for (let i = 0; i < itemAmount; i++) {
+        console.log("i: ", i);
+        itemsCarouselArray.push(<ItemCard key={i} id={i}/>)
+    }
+
+    console.log("itemsCarouselArray: ", itemsCarouselArray);
+
+    return (
+        <div>
+            { itemsCarouselArray.length > 1 ? <h2>Enter Item Info</h2> : <h2></h2>}
+            <Carousel interval={null} onChange={handleCarouselChange}>
+                {itemsCarouselArray}
+            </Carousel>
+            <br />
+            <br />
+            <br />
+            { itemsCarouselArray.length > 0 ? <Button variant="contained" color="primary" type="submit" onClick={handleItemsCarouselFormSubmit}>Submit All Items</Button> : null}
+        </div>
+    )
+}
 
 function ItemsForm({ moves, itemMoveSelectTagValue }) {
     const [itemAmount, setItemAmount] = useState(0);
