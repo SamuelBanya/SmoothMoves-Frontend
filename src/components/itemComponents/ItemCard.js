425c2a610c51b63346957c163d66660ea579e844
Samuel Banya <sbanya@fastmail.com>
2022-10-12 16:16:33 -0500

    Including 'item_id' to help update object array with correct value

--- src/components/itemComponents/ItemCard.js
+++ src/components/itemComponents/ItemCard.js
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 function ItemCard({ id, collectItemFormData }) {
     const [itemFormData, setItemFormData] = useState({
         // NOTE: Added an 'id' value to distinguish each 'ItemCard' instance from each other:
+        item_id: 0,
         name: "",
         length: 0, 
         width: 0,
@@ -17,14 +18,16 @@ function ItemCard({ id, collectItemFormData }) {
         const name = e.target.name;
         let value = e.target.value;
 
+        console.log("LOOKING AT handleItemFormChange: ");
+        console.log("itemFormData: ", itemFormData);
+        console.log("id in child ItemCard component within handleItemFormChange: ", id);
+
         setItemFormData({
             ...itemFormData,
             [name]: value,
             // NOTE: Here we are using the 'id' value so that we can identify this formData to update accordingly
             // when we aggregate the data in the parent ItemsForm component:
-            // NOTE: This is showing up as the value itself, so I'm not sure how I can create a new 'item_id'
-            // key itself
-            [id]: id
+            item_id: id 
         });
 
         // NOTE: This ie being used to aggregate all of the 'itemFormData' from all child ItemCard components:
